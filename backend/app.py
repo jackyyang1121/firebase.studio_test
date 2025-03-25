73cb242f46ec0474bf1882b6a6db14a50f31b7ba
鄭名洋 <mingyongpig@gmail.com>
2025-03-25 09:27:32 +0000

    modify

--- backend/app.py
+++ backend/app.py
@@ -88,10 +88,7 @@ def learning_progress():
     plans = LearningPlan.query.filter_by(user_id=current_user.id).all()
     return jsonify([{'goal': plan.goal, 'plan': plan.plan, 'created_at': str(plan.created_at)} for plan in plans]), 200
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
+with app.app_context():
+    db.create_all()
     
 #python backend/app.py
